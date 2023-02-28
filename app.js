5d75e90148c9ddd34f17506e1199aa644ad0b74e
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-02-28 23:57:54 +0700

    docs: adding comments in main() function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const numbers = [];
 
 async function main() {
   while (true) {
+    // Meminta pengguna untuk memilih menu dari daftar menu yang tersedia menggunakan method inquirer.prompt
     const { choice } = await inquirer.prompt({
       type: "list",
       name: "choice",
@@ -29,14 +30,17 @@ async function main() {
     });
 
     if (choice === 1) {
+      // jika memilih menu nomor 1, maka akan meminta untuk memasukkan jumlah angka yang ingin di input
       const { count } = await inquirer.prompt({
         type: "number",
         name: "count",
         message: "Masukkan jumlah angka yang ingin dimasukkan:",
       });
+      // Jika variabel count = false || undefined, menampilkan pesan "Input salah, silakan coba lagi!" di console
       if (!count) {
         console.log("Input salah, silakan coba lagi!");
       } else {
+        // Jika count adalah sebuah angka, menapilkan prompt untuk memasukkan dan menyimpan angka-angka tersebut di dalam array numbers
         for (let i = 0; i < count; i++) {
           const { number } = await inquirer.prompt({
             type: "number",
@@ -45,12 +49,15 @@ async function main() {
           });
           numbers.push(number);
         }
+        // Menampilkan list angka yang sudah diinput di console
         console.log(`List angka saat ini : ${numbers.join(", ")} \n`);
       }
     } else if (choice === 2) {
+      // Jika memilih menu nomor 2, melakukan pengurutan angka-angka yang sudah diinput menggunakan fungsi sorting dan menampilkan hasilnya di console
       const sortedNumbers = sorting(numbers);
       console.log(`Hasil sorting: ${sortedNumbers.join(", ")} \n`);
     } else if (choice === 3) {
+      // Jika memilih menu nomor 3,  menapilkan prompt untuk memasukkan sebuah angka yang ingin dicari di dalam array numbers menggunakan fungsi searching
       const { key } = await inquirer.prompt({
         type: "number",
         name: "key",
@@ -59,12 +66,14 @@ async function main() {
 
       const index = searching(numbers, key);
 
+      // Jika angka yang dicari ditemukan, menampilkan pesan "Angka [key] ditemukan pada index ke-[index]." di console, jika tidak ditemukan, menampilkan pesan "Angka [key] tidak ditemukan." di console
       if (index !== -1) {
         console.log(`Angka ${key} ditemukan pada index ke-${index}. \n`);
       } else {
         console.log(`Angka ${key} tidak ditemukan. \n`);
       }
     } else if (choice === 4) {
+      // Jika memilih menu nomor 4, akan menutup program
       console.log("Terima kasih!");
       break;
     }
